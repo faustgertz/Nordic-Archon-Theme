6714375c83f401ab2861a772292817de8d6f3a56
Faust Gertz <faust@faustgertz.com>
2012-03-07 21:31:41 -0600

    Update 'About Nordic' content

--- themes/luther/about.inc.php
+++ themes/luther/about.inc.php
@@ -6,26 +6,22 @@ if($_REQUEST['f'] == 'pdfsearch')
 	return;
 }
 ?>
-<h1 id="titleheader">About Nordic</h1>
-<p>Nordic is a catalog of archival materials from Luther College and Vesterheim Norwegian-American Museum.  It was designed to allow researchers, students, genealogists, and other interested users to search and browse the original materials that document the Norwegian-American experience.</p>
+<h1 id="titleheader">About <abbr title="Norwegian-American Digital Catalog">Nordic</abbr></h1>
+<p>This website is named Nordic (<strong>Nor</strong>wegian-American <strong>Di</strong>gital <strong>C</strong>atalog) and it is a result of many hours of work spent inventorying archival materials down to the folder level. Much like a library catalog, this catalog will only tell users what materials are held at the archives and where they are located. The majority of these letters, reports, and papers are not digitized and only a small amount of photographs are available to search and browse under “Digital Images.” Future plans include adding more digital content and images and also to explore the inclusion of other Scandinavian-American materials from additional repositories and locations. </p>
 
-
-
-<h2>About <abbr title="Norwegian-American Digital Catalog">Nordic</abbr></h2>
-<p>This website is named Nordic (<strong>Nor</strong>wegian-American <strong>Di</strong>gital <strong>C</strong>atalog of <strong>A</strong>rchives) and it is a result of many hours of work spent inventorying archival materials down to the folder level.  Much like a library catalog, this catalog will only tell users what materials are held at the archives and where they are located.  The majority of these letters, reports, and papers are not digitized and only about 100 photographs are available to search and browse under “Digital Images.”  It is hoped that in the future, we will be able to add more digital content and images and will also expand to include other Scandinavian-American materials from additional repositories and locations.</p>
 
 <h2>About “Journeys to America”</h2>
-<p>In June 2011, Luther College in partnership with Vesterheim Norwegian-American Museum received a grant from the National Historical Publications and Records Commission to create an aggregate, online catalog of materials from both repositories.  The project was named “Journeys to America” to emphasize the documentation of the immigration story.  
+<p>In June 2011, Luther College in partnership with Vesterheim Norwegian-American Museum received a grant from the National Historical Publications and Records Commission to create an aggregate, online catalog of materials from both repositories. The project was named “Journeys to America” to emphasize the documentation of the immigration story.  
 <a href="http://lis.luther.edu/files/project%20summary.pdf">Download the Project Summary</a>.</p>
 
 <h2>About Luther College &lt;<a href="http://www.luther.edu/">www.luther.edu</a>&gt;</h2>
-<p>Located in the small northeast Iowa town of Decorah, Luther College is an undergraduate liberal arts institution of about 2,500 students. The college is affiliated with the Evangelical Lutheran Church in America.  Founded in 1861 by Norwegian immigrants, Luther offers more than 60 majors and preprofessional and certificate programs leading to the bachelor of arts degree. Its mission statement emphasizes faith, leadership, and community service. Luther College provides equal opportunity for all qualified persons in its educational programs and activities.</p>
+<p>Located in the small northeast Iowa town of Decorah, Luther College is an undergraduate liberal arts institution of about 2,500 students. The college is affiliated with the Evangelical Lutheran Church in America. Founded in 1861 by Norwegian immigrants, Luther offers more than 60 majors and preprofessional and certificate programs leading to the bachelor of arts degree. Its mission statement emphasizes faith, leadership, and community service. Luther College provides equal opportunity for all qualified persons in its educational programs and activities.</p>
 
 <h2>About Vesterheim Norwegian-American Musuem &lt;<a href="http://www.vesterheim.org/">vesterheim.org</a>&gt;</h2>
-<p>Vesterheim (“Western Home”) Norwegian-American Museum is an AAM-accredited museum and cultural center that explores the diversity of American imigration through the lens of the Norwegian-American experience, incorporating its 16 historic buildings and over 24,000 artifacts with contemporary classes in Norwegian folk art and culture.  Vesterheim is an independent, not-for-profit organization and is the most comprehensive museum in the United States dedicated to a single immigrant group.</p>
+<p>Vesterheim (“Western Home”) Norwegian-American Museum is an AAM-accredited museum and cultural center that explores the diversity of American imigration through the lens of the Norwegian-American experience, incorporating its 16 historic buildings and over 24,000 artifacts with contemporary classes in Norwegian folk art and culture. Vesterheim is an independent, not-for-profit organization and is the most comprehensive museum in the United States dedicated to a single immigrant group.</p>
 
 <h2>About the NHPRC &lt;<a href="http://www.archives.gov/nhprc/">www.archives.gov/nhprc/</a>&gt;</h2>
-<p>The National Historical Publications and Records Commission (<abbr title="National Historical Publications and Records Commission">NHPRC</abbr>) is a statutory body affiliated with the National Archives and Records Administration (<abbr title="National Archives and Records Administration">NARA</abbr>) that supports a wide range of activities to preserve, publish, and encourage the use of documentary sources, created in every medium ranging from quill pen to computer, relating to the history of the United States.  NHPRC grants are vital to the preservation and accessibility of America’s memory and is essential in establishing the rights of citizens, ensuring the accountability of our government, and preserving the voice of national events as evidenced through the lives of our country’s people, communities, and organizations.</p>
+<p>The National Historical Publications and Records Commission (<abbr title="National Historical Publications and Records Commission">NHPRC</abbr>) is a statutory body affiliated with the National Archives and Records Administration (<abbr title="National Archives and Records Administration">NARA</abbr>) that supports a wide range of activities to preserve, publish, and encourage the use of documentary sources, created in every medium ranging from quill pen to computer, relating to the history of the United States. NHPRC grants are vital to the preservation and accessibility of America’s memory and is essential in establishing the rights of citizens, ensuring the accountability of our government, and preserving the voice of national events as evidenced through the lives of our country’s people, communities, and organizations.</p>
 
 <h2>Connect with the Luther College Archives!</h2>
 <ul>
@@ -42,13 +38,13 @@ if($_REQUEST['f'] == 'pdfsearch')
    <span class="fn">Sasha Griffin</span>, <span class="title">Project Archivist</span>
  </div>
  <a class="email" href="mailto:sgriffin@luther.edu">sgriffin@luther.edu</a>
- <div class="tel">563-387-1725</div>
+ <div class="tel">563/387-1725</div>
 </li>
 <li id="hcard-Sasha-Griffin" class="vcard">
  <div>
    <span class="fn">Rachel Vagts</span>, <span class="title">Project Director</span>
  </div>
  <a class="email" href="mailto:vagtsrac@luther.edu">vagtsrac@luther.edu</a>
- <div class="tel">563-387-1805</div>
+ <div class="tel">563/387-1805</div>
 </li>
 </ul>
