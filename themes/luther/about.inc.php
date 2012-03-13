afdeefc6baf053f5387cdd2125f6518bd90ec5a6
Faust Gertz <faust@faustgertz.com>
2012-03-13 07:59:17 -0500

    Minor changes to about, mainly to URLs in headers

--- themes/luther/about.inc.php
+++ themes/luther/about.inc.php
@@ -14,13 +14,13 @@ if($_REQUEST['f'] == 'pdfsearch')
 <p>In June 2011, Luther College in partnership with Vesterheim Norwegian-American Museum received a grant from the National Historical Publications and Records Commission to create an aggregate, online catalog of materials from both repositories. The project was named “Journeys to America” to emphasize the documentation of the immigration story.  
 <a href="http://lis.luther.edu/files/project%20summary.pdf">Download the Project Summary</a>.</p>
 
-<h2>About Luther College &lt;<a href="http://www.luther.edu/">www.luther.edu</a>&gt;</h2>
+<h2>About Luther College <a href="http://www.luther.edu/">www.luther.edu</a></h2>
 <p>Located in the small northeast Iowa town of Decorah, Luther College is an undergraduate liberal arts institution of about 2,500 students. The college is affiliated with the Evangelical Lutheran Church in America. Founded in 1861 by Norwegian immigrants, Luther offers more than 60 majors and preprofessional and certificate programs leading to the bachelor of arts degree. Its mission statement emphasizes faith, leadership, and community service. Luther College provides equal opportunity for all qualified persons in its educational programs and activities.</p>
 
-<h2>About Vesterheim Norwegian-American Musuem &lt;<a href="http://www.vesterheim.org/">vesterheim.org</a>&gt;</h2>
+<h2>About Vesterheim Norwegian-American Musuem <a href="http://www.vesterheim.org/">vesterheim.org</a></h2>
 <p>Vesterheim (“Western Home”) Norwegian-American Museum is an AAM-accredited museum and cultural center that explores the diversity of American imigration through the lens of the Norwegian-American experience, incorporating its 16 historic buildings and over 24,000 artifacts with contemporary classes in Norwegian folk art and culture. Vesterheim is an independent, not-for-profit organization and is the most comprehensive museum in the United States dedicated to a single immigrant group.</p>
 
-<h2>About the NHPRC &lt;<a href="http://www.archives.gov/nhprc/">www.archives.gov/nhprc/</a>&gt;</h2>
+<h2>About the NHPRC <a href="http://www.archives.gov/nhprc/">www.archives.gov/nhprc </a></h2>
 <p>The National Historical Publications and Records Commission (<abbr title="National Historical Publications and Records Commission">NHPRC</abbr>) is a statutory body affiliated with the National Archives and Records Administration (<abbr title="National Archives and Records Administration">NARA</abbr>) that supports a wide range of activities to preserve, publish, and encourage the use of documentary sources, created in every medium ranging from quill pen to computer, relating to the history of the United States. NHPRC grants are vital to the preservation and accessibility of America’s memory and is essential in establishing the rights of citizens, ensuring the accountability of our government, and preserving the voice of national events as evidenced through the lives of our country’s people, communities, and organizations.</p>
 
 <h2>Connect with the Luther College Archives!</h2>
